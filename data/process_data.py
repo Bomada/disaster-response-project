6ecb6744e89979a09d4ed39064fb3ca8ee5a4f18
Marcus Nilsson <marcusnilsson78@gmail.com>
2019-06-19 22:06:27 +0200

    feat: Create ETL pipeline

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,84 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """
+    Load messages and categories from csv files into a pandas dataframe.
+
+    Arguments:
+    messages_filepath -- path to location of messages csv file
+    categories_filepath -- path to location of categories csv files
+
+    Returns:
+    df -- pandas dataframe with messages and categories
+    """
+    # load messages dataset
+    messages = pd.read_csv(messages_filepath)
+
+    # load categories dataset
+    categories = pd.read_csv(categories_filepath)
+
+    # merge datasets
+    df = pd.merge(messages, categories, how='outer', on='id')
+
+    return df
 
 
 def clean_data(df):
-    pass
+    """
+    Clean data in a pandas dataframe.
+
+    Clean data in a pandas dataframe by renaming category columns, convert
+    category values to 0 or 1 and drop duplicates.
+
+    Arguments:
+    df -- pandas dataframe with messages and categories in source format
+
+    Returns:
+    df -- cleaned pandas dataframe with messages and categories
+    """
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(pat=';', expand=True)
+
+    # use first row to extract a list of new column names for categories
+    row = categories.iloc[0,]
+    category_colnames = []
+    for category in row:
+        category_colnames.append(category[0:-2])
+
+    # rename the columns of categories
+    categories.columns = category_colnames
+
+    # convert category values to 0 or 1
+    for column in categories:
+        categories[column] = categories[column].str[-1].astype(int)
+
+    # replace categories column in df with new category columns
+    df = df.drop('categories', axis=1)
+    df = pd.concat([df, categories], axis=1)
+
+    # drop duplicates
+    df = df.drop_duplicates()
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    """
+    Save data from a pandas dataframe into SQLite database.
+
+    Arguments:
+    df -- pandas dataframe with messages and categories
+    database_filename -- path to location of database file
+
+    Returns:
+    None
+    """
+    # save dataset into database
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql('message', engine, index=False, if_exists='replace')
 
 
 def main():
@@ -24,12 +92,12 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
